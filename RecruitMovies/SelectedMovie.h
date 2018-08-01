e339a406c60d7d6a15fe837ae17bf8c515f61c01
nguyen dinh phu <nguyendinhphu2909@gmail.com>
2018-08-01 09:09:58 +0700

    add SelectedMovie

---
+++ RecruitMovies/SelectedMovie.h
@@ -0,0 +1,13 @@
+//
+//  SelectedMovie.h
+//  RecruitMovies
+//
+//  Created by RTC-HN156 on 8/1/18.
+//  Copyright © 2018 RTC-HN156. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectedMovie : UIView
+
+@end
---
+++ RecruitMovies/SelectedMovie.m
@@ -0,0 +1,21 @@
+//
+//  SelectedMovie.m
+//  RecruitMovies
+//
+//  Created by RTC-HN156 on 8/1/18.
+//  Copyright © 2018 RTC-HN156. All rights reserved.
+//
+
+#import "SelectedMovie.h"
+
+@implementation SelectedMovie
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
